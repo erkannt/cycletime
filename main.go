a88db6cd793bfbd58578f8de17a2be13db75963a
Daniel Haarhoff <daniel@rknt.de>
2023-08-16 11:55:06 +0100

    Calculate duration by reducing of the git log backwards in time

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
+
+type cycle struct {
+	issue    string
+	end      time.Time
+	duration time.Duration
+}
 
 func main() {
 
@@ -23,16 +31,40 @@ func main() {
 		os.Exit(1)
 	}
 
-	commits, err := repo.Log(&git.LogOptions{})
+	commits, err := repo.Log(&git.LogOptions{Order: git.LogOrderCommitterTime})
 	if err != nil {
 		fmt.Printf("Failed to load git log:\n%s\n", err)
+		os.Exit(1)
 	}
 
-	var cCount int
+	cycles := make(map[string]cycle)
+
+	issueRegex, _ := regexp.Compile("#([0-9]+)")
+
 	commits.ForEach(func(c *object.Commit) error {
-		cCount++
+		issue := issueRegex.Find([]byte(c.Message))
+		if issue == nil {
+			return nil
+		}
+
+		existingCycle, cycleKnown := cycles[string(issue)]
+		var end time.Time
+		if cycleKnown {
+			end = existingCycle.end
+		} else {
+			end = c.Committer.When
+		}
+
+		cycles[string(issue)] = cycle{
+			issue:    string(issue),
+			end:      end,
+			duration: end.Sub(c.Committer.When),
+		}
+
+		newCycle := cycles[string(issue)]
+		fmt.Printf("%s %.0fh\n", newCycle.issue, newCycle.duration.Hours())
+
 		return nil
 	})
 
-	fmt.Println("Commit count:", cCount)
 }
